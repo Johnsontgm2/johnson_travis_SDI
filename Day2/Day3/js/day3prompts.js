19b755f8553109327c2a27063e3155d9312e2057
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-03-07 15:27:37 -0500

    Day 3 Prompts
    Learn to prompt the user to enter information that we can use

---
+++ Day2/Day3/js/day3prompts.js
@@ -0,0 +1,36 @@
+/*
+Travis Johnson
+Section 00
+03/07/15
+Day 3 Prompts
+ */
+
+//alert("Testing");
+
+//Ask the user for input
+//prompt("Text that the user will see")
+
+var userInput = prompt("Enter year of birth");
+console.log(userInput);
+
+//Lets calculate the area of a rectangle
+//length * width
+
+//Prompt the user for the width and height
+//Explain to them whats going on.
+// \n = new line
+var height = prompt("Lets calculate the area of a rectangle.\n Please enter the height of the rectangle: ");
+var width = prompt ("Please enter the width of the rectangle");
+
+//calculate the area of the rectangle
+var area = width*height;
+
+//Tell the use the answer
+console.log("The are of the rectangle with a height of " + height + " and a width of " + width + " is " + area);
+
+var results = "The results of the area are " + area;
+console.log(results);
+
+
+
+
