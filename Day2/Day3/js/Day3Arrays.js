dcb1fb274266b3079a51bb8c6d7438e0731e4da2
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-03-07 14:43:13 -0500

    learn basics of arrays
    basics of arrays, length property, avengers name

--- Day2/Day3/js/Day3Arrays.js
+++ Day2/Day3/js/Day3Arrays.js
@@ -6,3 +6,47 @@ Day 3 Arrays
  */
 //alert("Testing to make sure it is linked");
 
+//Create a basic array
+var avengersNames=["Thor", "Hulk", "Iron Man", "Capt. America" ];
+
+//Print out whole array
+console.log(avengersNames);
+
+//Print out one specific item in the array. ARRAY STARTS AT index number 0!!!!****
+console.log(avengersNames[2]);
+
+//Swap items in our away
+avengersNames[1]="Black Widow";
+
+console.log(avengersNames);
+
+//Use a variable for the index number
+var num = 3;
+console.log(avengersNames[num]);
+
+//Find the length of an array
+//Length property = How many items are in the array
+//dot syntax = fancy for use a period
+console.log(avengersNames.length);
+
+//Add an item to an array
+avengersNames[4]="Hawkeye";
+
+avengersNames[avengersNames.length] = "Wolverine";
+
+console.log(avengersNames);
+
+
+//Picking oranges
+var orangesPicked=[13,350,1000];
+
+//How many oranges did we pick in total
+var totalOranges=orangesPicked[0] + orangesPicked[1] + orangesPicked[2];
+console.log("The total number of orange picked is "+totalOranges+".");
+//can use orangesPicked.length instead of 3 but it has to be fixed up ^^ there ^^
+var averageOranges=totalOranges/3;
+console.log("The average numbers of oranges picked per day is " + averageOranges +".");
+
+
+
+
