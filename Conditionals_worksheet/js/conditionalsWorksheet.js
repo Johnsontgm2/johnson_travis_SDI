0b5780ce9309672d6a87c052f19ee7966bcfcd3b
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-03-12 11:55:26 -0400

    problem 2
    problem 2

--- Conditionals_worksheet/js/conditionalsWorksheet.js
+++ Conditionals_worksheet/js/conditionalsWorksheet.js
@@ -14,7 +14,7 @@ Conditionals Worksheet
  var milesPerGallon = 15;
  var distanceToGo = 200;
  var gasTankCapacity = 22;
- var gasTankPercentLeft = 25;
+ var gasTankPercentLeft = 50
  var gasTankPercentLeftDecimal = gasTankPercentLeft / 100;
  var gallonsLeft = gasTankPercentLeftDecimal * gasTankCapacity;
 
@@ -22,10 +22,44 @@ Conditionals Worksheet
 if(gasTankCapacity * gasTankPercentLeftDecimal * milesPerGallon >= distanceToGo){
     console.log("Yes, you can make it without getting gas.");
 
-} else{console.log("You only have " + gallonsLeft + " in your gas tank, better get gas while you can!");
+} else{console.log("You only have " + gallonsLeft + "gallons left in your gas tank, better get gas while you can!");
 }
 
- //
+ //Group2 question
+
+//set given variables
+ var userName = "spankiticus";
+ var Password = String(1234);
+ var enterUserName = prompt("Please enter your username");
+ var enterPassword = prompt("Please enter your password");
+
+ //setup conditional
+ if (enterUserName == userName){
+
+ } else (console.log("User not found"));
+
+ if (enterPassword == Password){
+
+
+ } else (console.log("Your password does not match our records"));
+
+ if (enterUserName == userName && enterPassword == Password){
+     console.log("Welcome Spankiticus")
+ }
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
