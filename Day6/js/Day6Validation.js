beff3b8835ac1b01325f7d91fc3c45f1a365ffee
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-03-14 17:27:18 -0400

    day 6 validation
    day 6 validation

--- Day6/js/Day6Validation.js
+++ Day6/js/Day6Validation.js
@@ -8,3 +8,36 @@ Day 3 Prompts
 
 
 //alert("Testing");
+
+//ask the user for a number
+ var num1 = prompt("Please enter a number");
+
+
+//validate that the user typed in a number OR didnt leave it blank
+
+ //validate using a while loop
+ while(isNaN(num1) || num1 === ""){
+     //Reprompt the user
+     if(num1 === ""){
+         //The user left it blank
+         num1 = prompt("Please do not leave blank\nPlease type in a number");
+
+     } else if(isNaN(num1)){
+         num1 = prompt("Please only type in numbers\nEnter a number");
+     }
+
+
+
+ }
+
+ //Ask the user a yes or no question
+ var userInput = prompt("Please enter yes or no");
+ //convert to lowercase
+ userInput = userInput.toLowerCase();
+
+ //validate
+ while(userInput != "yes" && userInput != "no" && userInput != "nope"){
+     //Re-prompt
+     userInput = prompt("Please only enter yes or no\n Please type in yes or no");
+
+ }
