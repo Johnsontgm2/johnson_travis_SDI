9e96467968416f2309790ab11bc78c92dcc2aa87
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-03-17 13:52:39 -0400

    Day7Beer
    using loops beer on the wall

--- Day7/js/Day7Beer.js
+++ Day7/js/Day7Beer.js
@@ -9,4 +9,22 @@ Day 3 Prompts
 
 //alert("Testing");
 
- 
+ //Code the song 99 bottles of beer on the wall
+
+
+
+ //basic structure of for loop
+ //for(var i = 0; condition to test; increment of change){}
+
+
+ for(var i= 99; i>0; i--) {
+
+     if(i===1){
+         console.log(" 1 bottle of beer on the wall. 1 bottle of beer. Take it down and pass it around. No more beer.")
+
+     } else{
+         console.log(i + " bottles of beer on the wall." + i + " bottles of beer. Take one down and pass it around " + (i - 1) + " bottles of beer on the wall");
+     }
+
+ }
+
