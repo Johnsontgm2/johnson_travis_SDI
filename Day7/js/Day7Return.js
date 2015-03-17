d0dff477268de7038d31034755cbf87149cd4d26
tmjohnson2@fullsail.edu <tmjohnson2@fullsail.edu>
2015-03-17 16:52:43 -0400

    Day 7 returns
    return variables

--- Day7/js/Day7Return.js
+++ Day7/js/Day7Return.js
@@ -2,9 +2,53 @@
  /*
 Travis Johnson
 Section 00
-03/07/15
-Day 3 Prompts
+03/17/15
+Day 7 returns
  */
 
 
-alert("Testing");
+//alert("Testing");
+
+
+ //Return a value from our function to our main code
+
+ //create a function calculate the area of a rectangle
+ function calcArea(w,h){
+     //calc area
+     var area = w * h;
+     console.log("Inside the function the area is " + area)
+     //Return the are to the main code
+     return area;
+
+
+ }
+
+ //call function
+ //create a variable to catch the returned value
+ var rectArea = calcArea(10,20);
+
+ //Console log the are
+ //console.log(area);
+ console.log(rectArea);
+
+
+ //Create a function to calculate the area of a circle
+ function circleArea (r){
+     //calc area Pi * r * r
+     var area = Math.PI * r * r;
+     //Return the value
+     return area;
+
+
+ }
+
+ //function call this circle area
+ //create a variable to catch the returned area
+ var circArea = circleArea(6);
+
+ //console.log the results
+ console.log("The are of the circle is " + circArea);
+
+ // What is twice the circle are of a six inch circle
+ var resultsTwice = circArea * 2;
+ console.log("Twice the area is " + resultsTwice);
